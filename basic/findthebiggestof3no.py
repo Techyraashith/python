111cd8cb99769f334c344d0ab19ca5f9166a3705
raashith <raashithmohammed305@gmail.com>
2022-04-01 23:21:27 -0800

    find the biggest no of 3

--- basic/findthebiggestof3no.py
+++ basic/findthebiggestof3no.py
@@ -1,12 +1,12 @@
-a=int(input("enter the first no:"))
-b=int(input("enter the second no:"))
-c=int(input("enter the third no:"))
-if a > b and b > c:
-    biggest = a 
-elif b > a and c > b:
-    biggest = b    
-else :                                           
-     biggest = c 
+n1=int(input("enter the first no:"))
+n2=int(input("enter the second no:"))
+n3=int(input("enter the third no:"))
+if n1 > n2  and n1 > n3:
+    biggest =n1 
+elif n2 > n1 and n2 > n3:
+    biggest = n2    
+else :                                       
+     biggest = n3 
 
 print("The biggest of the three no:",biggest)
     
